f14b27dbb09d1301c9548febc884796fbddd02ea
Abul Bashar <abulbashar98116@gmail.com>
2026-09-15 21:02:41 +0200

    used fixtures to avoid repetitive code, used parametrize and fixture together for test_bank_transaction function... finally used pytest.raises to expect sepcific Insuficcient funds error, created by inheriting exception and imported the class in test_calculations....

--- app/calculations.py
+++ app/calculations.py
@@ -10,12 +10,18 @@ def divide(num1: int, num2: 2):
 def multiply(num1: int, num2: int = 2):
     return num1 * num2
 
+class Insufficient_funds(Exception):
+    pass
+
 class BankAccount():
     def __init__(self,starting_balance=0):
         self.balance = starting_balance
     def deposit(self, amount):
         self.balance += amount
     def withdraw(self, amount):
+        if amount > self.balance:
+            raise Insufficient_funds("Insufficient funds")
+            # raise ZeroDivisionError
         self.balance -= amount
     def collect_interest(self):
         self.balance *= 1.1
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,14 @@
 import pytest
-from app.calculations import add,subtract,divide,multiply,BankAccount
+from app.calculations import add,subtract,divide,multiply,BankAccount,Insufficient_funds
+
+
+@pytest.fixture
+def uninitialized_bank_account():
+    return BankAccount()
+
+@pytest.fixture
+def initialized_bank_account_with_hundred():
+    return BankAccount(100)
 
 
 @pytest.mark.parametrize("num1, num2, expected", [
@@ -30,21 +39,35 @@ def test_set_initial_balance():
     bank_account = BankAccount(50)
     assert bank_account.balance == 50
 
-def test_starting_balance():
-    bank_account = BankAccount()
-    assert bank_account.balance == 0
+def test_default_amount(uninitialized_bank_account):
+    assert uninitialized_bank_account.balance == 0
 
-def test_deposit():
-    bank_account = BankAccount()
-    bank_account.deposit(25)
-    assert bank_account.balance == 25
+def test_deposit(initialized_bank_account_with_hundred):
+    # bank_account = BankAccount(15)
+    initialized_bank_account_with_hundred.deposit(25)
+    assert initialized_bank_account_with_hundred.balance == 125
 
-def test_withdraw():
-    bank_account = BankAccount(100)
-    bank_account.withdraw(50)
-    assert bank_account.balance == 50
+def test_withdraw(initialized_bank_account_with_hundred):
+    # bank_account = BankAccount(100)
+    initialized_bank_account_with_hundred.withdraw(50)
+    assert initialized_bank_account_with_hundred.balance == 50
 
-def test_collected_interest():
-    bank_account = BankAccount(50)
-    bank_account.collect_interest()
-    assert round(bank_account.balance, 5) == 55
+def test_collected_interest(initialized_bank_account_with_hundred):
+    # bank_account = BankAccount(50)
+    initialized_bank_account_with_hundred.collect_interest()
+    assert round(initialized_bank_account_with_hundred.balance, 5) == 110
+
+@pytest.mark.parametrize("deposited, withdrew, final_amount", [
+    (26, 13, 13),
+    (50, 21, 29),
+    (1500, 250, 1250),
+    (2100, 900, 1200)
+])
+def test_bank_transaction(uninitialized_bank_account, deposited, withdrew, final_amount):
+    uninitialized_bank_account.deposit(deposited)
+    uninitialized_bank_account.withdraw(withdrew)
+    assert uninitialized_bank_account.balance == final_amount
+
+def test_insufficient_funds(initialized_bank_account_with_hundred):
+    with pytest.raises(Insufficient_funds):
+        initialized_bank_account_with_hundred.withdraw(200)
