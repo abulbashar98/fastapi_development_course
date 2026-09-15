b4ab3567cd708cd28824512d5513481b20844ab7
Abul Bashar <abulbashar98116@gmail.com>
2026-09-15 15:20:52 +0200

    Created a BankAccount class, and test functions to test set_initial_balance,starting_balance,deposit method, withdraw method, and collected_interest method

--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculations import add,subtract,divide,multiply
+from app.calculations import add,subtract,divide,multiply,BankAccount
 
 
 @pytest.mark.parametrize("num1, num2, expected", [
@@ -26,3 +26,25 @@ def test_multiply():
     result = multiply(6,4)
     assert result == 24
 
+def test_set_initial_balance():
+    bank_account = BankAccount(50)
+    assert bank_account.balance == 50
+
+def test_starting_balance():
+    bank_account = BankAccount()
+    assert bank_account.balance == 0
+
+def test_deposit():
+    bank_account = BankAccount()
+    bank_account.deposit(25)
+    assert bank_account.balance == 25
+
+def test_withdraw():
+    bank_account = BankAccount(100)
+    bank_account.withdraw(50)
+    assert bank_account.balance == 50
+
+def test_collected_interest():
+    bank_account = BankAccount(50)
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 5) == 55
