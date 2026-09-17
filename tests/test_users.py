e323792888ab3477bc6213b0d93d6eecba8e28c1
Abul Bashar <abulbashar98116@gmail.com>
2026-09-17 19:01:32 +0200

    firstly we can store our create_user response in a variable, in this case new_user and pass it through pydantic schemas to validate respose for us..... then we created test_user_login function to test login route.... but the problem we face here first is that because test_user_login uses client fixture.. and client fixture defaulut scope is function scope, so when the test_user_login client is called from database.. it drops our previous database tables and create clean ones.. for that reason what we did is we changes both client fixture and session fixture scope to 'module'.. so it runs once once only each module.....

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,52 +1,5 @@
-from fastapi import FastAPI
-from fastapi.testclient import TestClient
-from app.main import app
 from app import schemas
-from app.config import settings
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from app.database import get_db, Base
-import pytest
-
-
-@pytest.fixture
-def session():
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-
-    db = TestingsessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-@pytest.fixture
-def client(session):
-    
-    def override_get_db():
-        try:
-            yield session
-        finally:
-            session.close()
-
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
-
-
-
-
-
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_ip_address}:{settings.database_port}/{settings.database_name}_test"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-TestingsessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
-
-
-
-
+from database import client,session
 
 
 
@@ -58,9 +11,17 @@ def test_root(client, session):
     assert res.status_code == 200
 
 def test_create_user(client):
-    res = client.post("/users", json={"email" : "alamin@email.com", "password" : "password1234"})
+    res = client.post("/users", json={"email": "hello123@gmail.com", "password": "password123"})
+
+    new_user = schemas.UserResponse(**res.json())
+    print(new_user)
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+
+def test_user_login(client):
+    res = client.post("/login", data={"username": "hello123@gmail.com", "password": "password123"})
 
     print(res.json())
-    new_user = schemas.UserResponse(**res.json())
-    assert new_user.email == "alamin@email.com"
-    assert res.status_code == 201
+    assert res.status_code == 200
+
+
